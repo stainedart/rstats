dc1d4aca377394638ae638963e48fb742948daf8
Carl Tremblay <ctremblay@outbox.com>
2018-05-04 08:57:06 -0400

    gitignore added

---
+++ src/project.R
@@ -0,0 +1,16 @@
+# Create a function to print squares of numbers in sequence.
+lowestValue <- function(a) {
+   #low
+   for(i in a) {
+   	 #if
+       print(i)
+   }
+}
+
+
+inputList <- round(runif(100,0,10000), 0)
+
+lowestValue(inputList)
+
+
+print ( inputList )
