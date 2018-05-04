b3772b7a0afd78062bcc7bc12122707c305e450b
Carl Tremblay <ctremblay@outbox.com>
2018-05-04 09:52:06 -0400

    Added lowest and highest number functions with print statements

--- src/project.R
+++ src/project.R
@@ -1,16 +1,31 @@
-# Create a function to print squares of numbers in sequence.
+# Function to pring the lowest value of the provided list.
 lowestValue <- function(a) {
-   #low
+   low <- a[1]
    for(i in a) {
-   	 #if
-       print(i)
+   	 if(i < low){
+         print(paste(i, "is lower than", low))
+       low <- i
+     }
    }
+    print(paste("Lowest value:", low))
+}
+
+# Function to pring the highest value of the provided list.
+highestValue <- function(a) {
+    high <- a[1]
+    for(i in a) {
+        if(i > high){
+            print(paste(i,"is higher than",high))
+            high <- i
+        }
+    }
+    print(paste("Highest value:", high))
 }
 
 
 inputList <- round(runif(100,0,10000), 0)
+print ( inputList )
 
 lowestValue(inputList)
+highestValue(inputList)
 
-
-print ( inputList )
