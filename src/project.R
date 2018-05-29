465e8d1b68d94ac460d4050153536cbb46a1ab64
Carl Tremblay <ctremblay@outbox.com>
2018-05-29 19:15:17 -0400

    Removed temporarily the max and min functions since they are useless now that the list is sorted.

--- src/project.R
+++ src/project.R
@@ -1,30 +1,30 @@
 # Function to pring the lowest value of the provided list.
-lowestValue <- function(a) {
-   low <- a[1]
-   for(i in a) {
-   	 if(i < low){
-        if(debug){
-            print(paste(i, "is lower than", low))
-        }
-       low <- i
-     }
-   }
-    print(paste("Lowest value:", low))
-}
+# lowestValue <- function(a) {
+#    low <- a[1]
+#    for(i in a) {
+#    	 if(i < low){
+#         if(debug){
+#             print(paste(i, "is lower than", low))
+#         }
+#        low <- i
+#      }
+#    }
+#     print(paste("Lowest value:", low))
+# }
 
 # Function to pring the highest value of the provided list.
-highestValue <- function(a) {
-    high <- a[1]
-    for(i in a) {
-        if(i > high){
-            if(debug){
-                print(paste(i,"is higher than",high))
-            }
-            high <- i
-        }
-    }
-    print(paste("Highest value:", high))
-}
+# highestValue <- function(a) {
+#     high <- a[1]
+#     for(i in a) {
+#         if(i > high){
+#             if(debug){
+#                 print(paste(i,"is higher than",high))
+#             }
+#             high <- i
+#         }
+#     }
+#     print(paste("Highest value:", high))
+# }
 
 #Function returning the absolute value of a given number.
 absoluteValue <- function(number){
@@ -124,6 +124,8 @@ sortBasic <- function(numberList){
     if(debug) {
         print(sortedList)
     }
+    print(paste("Highest value:",sortedList[0]))
+    print(paste("Lowest value:",sortedList[length(sortedList)]))
     return(sortedList)
 }
 
@@ -198,8 +200,8 @@ maxValue <- 100
 inputList <- round(runif(numberOfDigits,0,maxValue), 0)
 print ( inputList )
 
-lowestValue(inputList)
-highestValue(inputList)
+#lowestValue(inputList)
+#highestValue(inputList)
 mode(inputList)
 #calulated during the standard deviation calculation.
 #average(inputList)
